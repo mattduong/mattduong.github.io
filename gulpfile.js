ff8f929e46882d98d6eb11bbe9168cc9998722d3
mattduong <matt.duong@gmail.com>
2021-02-28 08:53:30 -0500

    gulp updates

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const { src, dest, watch, series} = require('gulp');
+const sass = requie('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+//sass
+function scssTask(){
+    return src('scss/style.scss', {sourcemaps:true})
+      .pipe(sass())
+      .pipe(postcss(cssnano))
+      .pipe(dest('dist', {sourcemaps:'.'}));
+}
+
+//js
+function jsTask(){
+    return src('js/script.js', { sourcemaps: true})
+      .pipe(terser())
+      .pipe(dest('dist', {sourcemaps:'.'}));
+}
+
+//browersync
+function browsersyncServe(cb){
+  browsersync.init({
+    server: {
+      baseDir: '.'
+    }
+  });
+  cb();
+}
+
+function browsersyncReload(cb){
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask(){
+  watch('*.html', browsersyncReload);
+  watch(['scss/**/*.scss', 'js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
+}
+
+// Default Gulp task
+exports.default = series(
+  scssTask,
+  jsTask,
+  browsersyncServe,
+  watchTask
+);
